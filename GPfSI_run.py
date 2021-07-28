5b2bd38ee3323c856448fbfdf736594c6875a044
Jinwoousc <42730000+Jinwoousc@users.noreply.github.com>
2021-07-28 11:20:13 -0400

    Add files via upload

--- GPfSI_run.py
+++ GPfSI_run.py
@@ -1,22 +1,22 @@
 # '''
 # @file GPfSI_run.py
-# @brief Python file to configure and excute GPfSI 
+# @brief Python file to configure and excute GPfSI
 # Copyright (C) 2021 Jinwoo Im
 # Sonny Astani Department of Civil and Environmental Engineering, University of Southern California, Los Angeles, USA
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-# 
+#
 # This is the Python file to configure and execute Genetic Programing for System Identification (GPfSI).
 # '''
 
@@ -29,6 +29,10 @@ import numpy as np
 import sympy
 from sympy import *
 import GPfSI
+import GPfSI_expressiontree as GPfSI_et
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
+import csv
 
 
 
@@ -170,12 +174,12 @@ expressiontree_parameters = saved_configuration[3]
 
 # Loss function over time
 
-mildstones=[]    
-print('each seesion end')   
+mildstones=[]
+print('each seesion end')
 for i in generation_offsets:
     mildstones.append(bestequations[i-1])
     print(f'best at gen. {i}: {bestequations[i-1].eqn_print()} // {bestfitness[i-1]}')
-    
+
 complexity = []
 for j in range(len(bestequations)):
     complexity.append(GPfSI_et.counting_components(bestequations[j]))
@@ -212,8 +216,8 @@ print('top 10 models w/ loss function values')
 for i in range(10):
     print(f'{i+1}: {equations[i].eqn_print()} // {fitness_info[i]}')
 
-# texts save to csv    
-    
+# texts save to csv
+
 with open(f'results/result_outline_case_{case}.csv', 'w', newline='') as csvfile:
     result_csv = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
     result_csv.writerow(['Model 1 is r(X, Xt) in "f(t) = m*Xtt + r(X, Xt)"'])
@@ -226,5 +230,4 @@ with open(f'results/result_outline_case_{case}.csv', 'w', newline='') as csvfile
     result_csv.writerow(['top 10 models w/ loss function values'])
     for i in range(10):
         result_csv.writerow([f'{i+1}: {equations[i].eqn_print()} // {fitness_info[i]}'])
-    csvfile.close()    
-    
+    csvfile.close()
